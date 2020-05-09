e15fcd4655a8d92749e3e30c1567a1cfa102a6f6
Mohammed <mms.dala@gmail.com>
2020-05-09 15:28:21 -0700

    create message tag in home.html

--- qarysite/users/views.py
+++ qarysite/users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('gary_post:list')
+            return redirect('qary_post:list')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
